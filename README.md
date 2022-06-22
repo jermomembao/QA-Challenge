271e8b78be0081787b22526f69921e232892a503
Jerry Mbao <mbaojerome17@gmail.com>
2022-06-22 23:18:49 +0100

    final push

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# QA-Challenge
+# Junior QA-Challenge
 Loan Amortise
 Small repo containing a word document 'Loan Amortise issues.doc' with a list of issues found on the Loan Amortise application.
+
+Also added my small attempt to automate using Wbdio and cucumber, although it was not fully running as I struggled a little identifying the `Calculate` button element to click.
--- automation-loans-tests/features/step-definitions/steps.js
+++ automation-loans-tests/features/step-definitions/steps.js
@@ -4,20 +4,22 @@ Given(/^I am on the amortiser application$/, async () => {
     await browser.url(`http://localhost:3000/`);
     await expect($('#loanAmount')).toBeExisting();
     await expect($('#loanTerm')).toBeExisting();
-    await expect($('#intereslRate')).toBeExisting();
+    await expect($('#interestRate')).toBeExisting();
 
 });
 
 When(/^I enter values (.*) (.*) (.*)$/, async (loanAmount, loanTerm, interestRate) => {
     await $('#loanAmount').setValue(loanAmount);
     await $('#loanTerm').setValue(loanTerm);
-    await $('#intereslRate').setValue(interestRate);
-    await $('button=Calculate').click();
+    await $('#interestRate').setValue(interestRate);
+    await $('[class=chakra-button css-f6n2t5]').click();
 });
 
 Then(/^I should see loan calculations for the period specified$/, async () => {
     await expect($('Monthly Repayment Amount')).toBeExisting();
     await expect($('Total Interest Repayable')).toBeExisting();
     await expect($('Total Amount Repayable')).toBeExisting();
+});
+
 
 
